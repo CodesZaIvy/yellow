e9ca519085a67eb8e2a5a231692111776a411e0e
CodesZaIvy <mosetiivy@gmail.com>
2024-08-06 12:20:06 +0300

    Generate pseudo-random numbers
    Generated random numbers for different variables

---
+++ src/main/java/org/color/RandomValues.java
@@ -0,0 +1,18 @@
+package org.color;
+
+import java.util.Random;
+
+public class RandomValues {
+    public static void main(String[] args) {
+        //Mostly used for game development
+
+        Random random = new Random();
+        // pseudo random number
+        // to limit the number have the number in the bracket
+        //int t = random.nextInt(9) + 1;
+        //double t = random.nextDouble();
+        boolean t = random.nextBoolean();
+        System.out.println(t);
+
+    }
+}
