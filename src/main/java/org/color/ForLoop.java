50d693a118bf8227215bc54b6ef0a86f91eb6151
CodesZaIvy <mosetiivy@gmail.com>
2024-08-15 17:12:05 +0300

    Use for loop
    Used for loop to specify number of times a condition can be executed

---
+++ src/main/java/org/color/ForLoop.java
@@ -0,0 +1,27 @@
+package org.color;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        /*
+        for(int i =0; i<=10; i++){
+
+            //int i=0 -> declare an index call it
+            //i<=10 -> iterate the for loop as long as the condition is true
+            //i++ ->add 1 to the number being iterated making it 11 as it starts from 0
+
+            System.out.println(i);
+        */
+
+        for(int i =10; i>=0; i-=2) {
+            // i-=2 --> enables condition to jump two number
+            System.out.println(i);
+
+        }
+
+            System.out.println("Happy Birthday Udaku");
+
+    }
+}
+
+
+
