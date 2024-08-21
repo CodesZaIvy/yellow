13c0eb4f8194b393463b337e6454aa090b9cee44
CodesZaIvy <mosetiivy@gmail.com>
2024-08-21 16:47:57 +0300

    Use Wrapper Class
    Provided a way to use primitive data types as objects.

---
+++ src/main/java/org/color/Wrapper.java
@@ -0,0 +1,45 @@
+package org.color;
+
+public class Wrapper {
+    public static void main(String[] args) {
+// Wrapper Class -> provide a way to use primitive data types as objects.
+    /*
+    Each primitive type has a corresponding wrapper class:
+        byte -> Byte
+        short -> Short
+        int -> Integer
+        long -> Long
+        float -> Float
+        double -> Double
+        char -> Character
+        boolean -> Boolean
+    Autoboxing/Unboxing: Java automatically converts between the primitive types and their corresponding wrapper class objects.
+    This is called autoboxing (primitive to wrapper) and unboxing (wrapper to primitive)
+     */
+ Boolean a = true;
+ Character b = 'd';
+ Integer c = 209;
+ Double d = 3.14;
+ String r = "Hers";
+
+ if (c==2090){
+     System.out.println("This is a true");
+
+ }else{
+     System.out.println("Try again");
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
