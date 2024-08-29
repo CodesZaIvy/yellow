1b2a6ce090397b29f78120b8c34b713f291ee4ce
CodesZaIvy <ivykwammboka@gmail.com>
2024-08-29 09:17:01 +0300

    Use of the Final Keyword Used to prevent method parameters from being reassigned within the method.

---
+++ src/main/java/org/color/FinalKeyword.java
@@ -0,0 +1,11 @@
+package org.color;
+
+public class FinalKeyword {
+    public static void main(String[] args) {
+        final int BIRTHDAY = 11;
+        //common practice to make a final method be in CAPS
+
+        System.out.println(BIRTHDAY);
+
+    }
+}
