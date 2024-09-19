7ce30b952157247f02b7bfa8ed2f56e5fe011667
CodesZaIvy <ivykwammboka@gmail.com>
2024-09-19 14:13:08 +0300

    Use of Access Modifiers
    Used access modifiers to control the visibility and accessibility of classes, methods, and variables

---
+++ src/main/java/org/color/AccessModifiers.java
@@ -0,0 +1,11 @@
+package org.color;
+
+public class AccessModifiers {
+
+    /*Public: Accessible from anywhere.
+            Private: Accessible only within the class.
+    Protected: Accessible within the package and by subclasses.
+            Default (Package-private): Accessible within the package, but not by subclasses outside the package.
+}
+
+     */
